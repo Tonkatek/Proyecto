b0aa47921c9ec8c7ebc9315a9262c8193b8f5fe7
Alejandro Casa <an658716@gmail.com>
2026-01-30 16:06:49 +0100

    empezando arquitectura MVC

---
+++ config/params.php
@@ -0,0 +1,15 @@
+<?php
+
+    return [
+        'app_title' => 'tonkatek_db',
+        
+        
+        'db' => [
+            'host'    => 'db',       
+            'user'    => 'admin',            
+            'pass'    => 'admin1234',                
+            'dbname'  => 'tonkatek_db', 
+            'charset' => 'utf8mb4'          
+        ]
+    ];
+?>
