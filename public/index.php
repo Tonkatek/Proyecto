c24688a7acb18f312ddf2ec985646d8f1d2306bc
pablogmsz <pablogmsanchez@gmail.com>
2026-01-31 17:46:34 +0100

    Creacion de controladores y edición de index + fallos XD

--- public/index.php
+++ public/index.php
@@ -1,4 +1,40 @@
 <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+</html>
+
+<?php
+    require_once "../src/libs/config.php";
+    require_once "../src/libs/View.php";
+    require_once "../src/libs/Database.php";
+    require_once "../src/libs/model.php";
+    
+    $controllerName = isset($_GET["controller"]) ? $_GET["controller"] : "Home";
+    $actionName = isset($_GET["action"]) ? $_GET["action"] : "index";
+
+    $controllerClassName = ucfirst($controllerName) . "Controller";
+    $fileController = "../src/controllers/$controllerClassName.php";
+
+    if (file_exists($fileController)) {
+        require_once $fileController;
+        $controller = new $controllerClassName();
+
+        if (method_exists($controller, $actionName)) {
+            $controller->$actionName();
+
+        } else {
+            die("Error: La acción \"$actionName\" no existe en \"$controllerClassName\".");
+        }
+
+    } else {
+        die("Error: El controlador \"$controllerClassName\" no existe.");
+    }
+?>
+<!-- <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
@@ -7,15 +43,15 @@
 </head>
 <body class="p-10">
     <?php
-        require_once "/var/www/src/libs/model.php";
-        try {
-            $claseConfig = new Model();
+        // require_once "/var/www/src/libs/model.php";
+        // try {
+        //     $claseConfig = new Model();
             
-        } catch (\Throwable $th) {
-            echo "algo ha salido mal";
-        }
+        // } catch (\Throwable $th) {
+        //     echo "algo ha salido mal";
+        // }
 
-        echo "Ha salido todo bien"
+        // echo "Ha salido todo bien"
     ?>
     <div class="card w-96 bg-base-100 shadow-xl">
         <div class="card-body">
@@ -28,4 +64,4 @@
     </div>
 
 </body>
-</html>
+</html> -->
