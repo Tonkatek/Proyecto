bacb977f61b01b1dd26d84d468b3d694516a7778
pablogmsz <pablogmsanchez@gmail.com>
2026-01-31 18:38:50 +0100

    Arreglando fallos

--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-</html>
-
 <?php
     require_once "../src/libs/config.php";
     require_once "../src/libs/View.php";
@@ -34,34 +25,3 @@
         die("Error: El controlador \"$controllerClassName\" no existe.");
     }
 ?>
-<!-- <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>PHP + Tailwind + DaisyUI</title>
-    <link href="./css/output.css" rel="stylesheet">
-</head>
-<body class="p-10">
-    <?php
-        // require_once "/var/www/src/libs/model.php";
-        // try {
-        //     $claseConfig = new Model();
-            
-        // } catch (\Throwable $th) {
-        //     echo "algo ha salido mal";
-        // }
-
-        // echo "Ha salido todo bien"
-    ?>
-    <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body">
-            <h2 class="card-title">¡Funciona! 🚀</h2>
-            <p class="font-bold">UwU</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">OK</button>
-            </div>
-        </div>
-    </div>
-
-</body>
-</html> -->
--- src/views/home/home.php
+++ src/views/home/home.php
@@ -1,4 +1,4 @@
-<!-- <div class="welcome-container" style="text-align: center; padding: 40px;">
+<div class="welcome-container" style="text-align: center; padding: 40px;">
     <h1><?php echo $titulo; ?></h1>
     <p><?php echo $texto; ?></p>
     <hr>
@@ -7,4 +7,4 @@
     <div style="margin-top: 30px;">
         <a href="index.php?controller=Prueba" class="btn" style="background: #2c3e50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Ir al test técnico de conexión</a>
     </div>
-</div> -->
+</div>
